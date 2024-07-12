cf3909d8220af5da180eae2a0caa8f8e57b979d6
unknown <mpwsd518@gmail.com>
2024-07-12 17:51:56 +0900

    プロジェクトをセットアップ

---
+++ src/main/java/com/sakashita/nameapp/NameappApplication.java
@@ -0,0 +1,13 @@
+package com.sakashita.nameapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NameappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sakashita/nameapp/NameappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakashita.nameapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
