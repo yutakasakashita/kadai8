2d36cd2ad480407d459d1bbf3552efb6d4b54086
unknown <mpwsd518@gmail.com>
2024-07-15 16:14:24 +0900

    DBを設定を作成

---
+++ sql/001-create-table-and-load-data.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS names;
+
+CREATE TABLE names (
+ id int unsigned AUTO_INCREMENT,
+ name VARCHAR(20) NOT NULL,
+ PRIMARY KEY(id)
+);
+INSERT INTO names (name) VALUES ('satou');
+INSERT INTO names (name) VALUES ('takahashi');
